b6224428e58c95d8ed17f5d5683b7fdc121b4dc5
Howakyl96 <howakyl@gmail.com>
2021-01-21 18:48:02 -0800

    function to check if snake hits a wall, or a div with snake class

--- app.js
+++ app.js
@@ -85,3 +85,17 @@ const moveSnake = (squares) => {
     eatApple(squares, tail);
     squares[currentSnake[0]].classList.add("snake");
 };
+
+const checkForHits = (squares) => {
+    if (
+        (currentSnake[0] + width >= (width*width) && direction === width) ||
+        (currentSnake[0] % width === width -1 && direction === 1) ||
+        (currentSnake[0] % width === 0 && direction === -1) ||
+        (currentSnake[0] - width <= 0 && direction === -width) ||
+        squares[currentSnake[0] + direction].classList.contains("snake")
+    ) {
+        return true
+    } else {
+        return false 
+    }
+};
